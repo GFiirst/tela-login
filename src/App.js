3c4d34bfc3d0380a37901da05597511bcd60a8dc
GFiirst <geovanefiirst@hotmail.com>
2023-05-20 17:40:03 -0400

    tela login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css'
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="container-login">
+        <div className="wrap-login">
+          <form className="login-form">
+            <span className="login-form-title">
+              Bem vindo!
+            </span>
+            <span className="login-form-title">
+              <img src="" alt= "logo-site"/>
+            </span>
+            <div className="wrap-input">
+              <input className="input" type= "email"/>
+              <span className="focus-input" data-placeholder = "email" > </span>
+            </div>
+            <div className="wrap-input">
+              <input className="input" type= "password"/>
+              <span className="focus-input" data-placeholder = "Senha" > </span>
+            </div>
+            <div className="container-login-form-btn">
+              <button className="login-form-btn">
+                login
+              </button>
+            </div>
+            <div className="text-register">
+              <span className="txt1">Não possui conta?</span>
+              <a className="txt2" href="#">Criar conta.</a>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
